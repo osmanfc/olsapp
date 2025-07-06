e70c566d905382cb4d1ff0a9450c2fb9c6c38682
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-07-06 17:36:42 +0600

    Update footer.php

--- layout/footer.php
+++ layout/footer.php
@@ -9,9 +9,18 @@
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
                             <div class="text-md-right footer-links d-none d-sm-block">
-                                <a href="javascript: void(0);">About</a>
-                                <a href="javascript: void(0);">Support</a>
-                                <a href="javascript: void(0);">Contact Us</a>
+                                <a href="https://olspanel.com/about">About</a>
+                                <a href="https://olspanel.com/contact">Contact Us</a>
+                                <?php 
+                                $versionInfo = $core->selfversion();
+                                if ($versionInfo['update_available']) {
+    $vtext= $versionInfo['local_version'] . ", Avaible: " . $versionInfo['remote_version'];
+} else {
+    $vtext= $versionInfo['local_version'];
+}
+                                
+                                ?>
+                                 <a href="https://olsapp.olspanel.com/">Version: <?=$vtext;?></a>
                             </div>
                         </div>
                     </div>
